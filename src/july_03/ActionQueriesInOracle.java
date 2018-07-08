e848aefaff176b8204a82f7ffe09cf42b135a559
MautKaSaudagar <krishnacaresu@gmail.com>
2018-07-08 12:52:07 +0530

    7 july

--- src/july_03/ActionQueriesInOracle.java
+++ src/july_03/ActionQueriesInOracle.java
@@ -2,6 +2,7 @@ package july_03;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -18,8 +19,21 @@ public class ActionQueriesInOracle {
 	static int updateValues(Connection con) throws SQLException{
 		Statement st = con.createStatement();
 		
-		return st.executeUpdate("UPDATE EMP SET EID=500 WHERE EID=101");
+		return st.executeUpdate("UPDATE EMP SET NAME='KMV' WHERE EID=105");
 	}
+	
+	static void showData(Connection con) throws SQLException{
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM EMP");
+		while(rs.next()){
+			System.out.println("eId : " + rs.getString(1) + ", name : " + rs.getString(2) + ", DId : " +rs.getString(3));
+		}
+	}
+	
+	static int removeData(Connection con) throws SQLException{
+		Statement st = con.createStatement();
+		return st.executeUpdate("DELETE FROM EMP WHERE EID=105");
+ 	}
 	
 	
 	public static void main(String[] args) throws SQLException {
@@ -30,10 +44,12 @@ public class ActionQueriesInOracle {
 		if(con != null){
 			System.out.println("Conection established");
 			int affectedRows = 0;
-			//affectedRows = insertValues(con);
-			// affectedRows = updateValues(con);
+			affectedRows = insertValues(con);
+			affectedRows = updateValues(con);
+			affectedRows = removeData(con);
+			showData(con);
 			
-			System.out.println("Affected Rows : " + affectedRows);
+			//System.out.println("Affected Rows : " + affectedRows);
 			
 			con.close();
 		}
